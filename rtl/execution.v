b793b45f54be29718d23db410cd7d9160015218a
diegob94 <dbim.bena@gmail.com>
2020-07-16 21:29:36 -0400

    Alu code improvement

--- rtl/execution.v
+++ rtl/execution.v
@@ -8,12 +8,7 @@ module arith_logic_unit (
     output reg [`DATA_WIDTH-1:0] alu_dout,
     output reg [`ALU_COMP_WIDTH-1:0] alu_comp
 );
-reg sign;
-wire eq;
-wire lt;
-wire ltu;
 always @(*) begin
-    sign = 0;
     alu_dout = 0;
     case (alu_op)
         `ALU_OP_NOP:  alu_dout = {`DATA_WIDTH{1'bx}};
@@ -25,44 +20,14 @@ always @(*) begin
         `ALU_OP_SRA:  alu_dout = $signed(alu_din1) >>> alu_din2[`ALU_SHIFT_DIN2_WIDTH-1:0];
         `ALU_OP_XOR:  alu_dout = alu_din1 ^ alu_din2;
         `ALU_OP_OR:   alu_dout = alu_din1 | alu_din2;
-        `ALU_OP_SLT:  alu_dout = lt;
-        `ALU_OP_SLTU: alu_dout = ltu;
+        `ALU_OP_SLT:  alu_dout = alu_comp[`ALU_COMP_LT];
+        `ALU_OP_SLTU: alu_dout = alu_comp[`ALU_COMP_LTU];
     endcase
 end
 always @(*) begin
-    alu_comp[`ALU_COMP_EQ]  = eq;
-    alu_comp[`ALU_COMP_LT]  = lt;
-    alu_comp[`ALU_COMP_LTU] = ltu;
+    alu_comp[`ALU_COMP_EQ]  = alu_din1 == alu_din2;
+    alu_comp[`ALU_COMP_LT]  = $signed(alu_din1) < $signed(alu_din2);
+    alu_comp[`ALU_COMP_LTU] = alu_din1 < alu_din2;
 end
-comparator comp(
-    .a(alu_din1),
-    .b(alu_din2),
-    .eq(eq),
-    .lt(lt),
-    .ltu(ltu)
-);
 endmodule
 
-module comparator(
-    input [`DATA_WIDTH-1:0] a,
-    input [`DATA_WIDTH-1:0] b,
-    output reg eq,
-    output reg gt,
-    output reg lt,
-    output reg equ,
-    output reg gtu,
-    output reg ltu
-);
-reg signed [`DATA_WIDTH-1:0] a_s;
-reg signed [`DATA_WIDTH-1:0] b_s;
-always @(*) begin
-    a_s = a;
-    b_s = b;
-    equ = a == b;
-    ltu = a < b;
-    gtu = a > b;
-    eq = a_s == b_s;
-    lt = a_s < b_s;
-    gt = a_s > b_s;
-end
-endmodule
