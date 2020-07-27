a390c15eab8f944702fffc045ed2fd14b065fd9f
diegob94 <dbim.bena@gmail.com>
2020-07-27 01:44:31 -0400

    Remove README.md regression results

--- scripts/unit_test.py
+++ scripts/unit_test.py
@@ -44,54 +44,12 @@ test_list_len = [len(i) for i in test_list]
 with Pool() as p:
     results = p.map(partial(run_test, max(test_list_len)+1), test_list)
 
+summary = dict(passed = 0, failed = 0, error = 0, total = len(results))
+for r in results:
+    summary[r['result']] += 1
+summary = {'passed': 43, 'failed': 0, 'error': 0, 'total': 43}
 
-def generate_report(df):
-    def add_res_col(df, res):
-        Res = res.strip().capitalize()
-        df[Res] = [Res if i == res else '' for i in df.result]
-        return df
-    df = add_res_col(df, 'passed')
-    df = add_res_col(df, 'failed')
-    df = add_res_col(df, 'error')
-    df = df.rename({'test':'Test'}, axis = 'columns')
-    N = len(df)
-    totals = dict(
-        Test = str(N), 
-        log = '',
-        result = '',
-        Passed = str(sum(df.result == 'passed')), 
-        Failed = str(sum(df.result == 'failed')), 
-        Error = str(sum(df.result == 'error'))
-    )
-    temp = {k:f'---' if k != 'Test' else 'Summary' for k in totals.keys()} 
-    df = df.append(temp, ignore_index = True)
-    df = df.append(totals, ignore_index = True)
-    temp = {k:f'{100*float(v)/N:.1f}%' if v != '' else '' for k,v in totals.items()} 
-    df = df.append(temp, ignore_index = True)
-    #index = list(df.index)
-    #index.insert(0, index[-1])
-    #index.insert(0, index[-2])
-    #index = index[:-2]
-    #df = df.reindex(index)
-    return df[['Test', 'result', 'Passed', 'Failed', 'Error', 'log']]
+print('\nSummary:')
+for r in summary.keys():
+    print(f'{r.capitalize():6s} {summary[r]:5d} {100*summary[r]/summary["total"]:6.2f}%')
 
-def display(test_report):
-    return test_report.rename({'Passed':'','Failed':'Result','Error':''}, axis = 'columns')
-
-df = pd.DataFrame(results)
-test_report = generate_report(df)
-test_report = test_report[['Test','Passed','Failed','Error']]
-print("\nSummary:")
-print(test_report.iloc[-2:,:].T.reindex(['Passed','Failed','Error','Test']).rename({'Test':'Total'},axis='index').to_string(header = False))
-
-readme = str(tabulate.tabulate(display(test_report), tablefmt = 'github', showindex = False, headers="keys"))
-
-readme_path = Path('../README.md')
-header = "## Unit test results:"
-text = []
-for line in readme_path.read_text().splitlines():
-    text.append(line)
-    if line.strip() == header:
-        break
-readme_path.write_text('\n'.join(text) + '\n\n' + readme + '\n\n')
-print('\nUpdated README.md')
