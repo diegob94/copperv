14e4e5589caa0899c10e15549bf578831176ae02
diegob94 <dbim.bena@gmail.com>
2022-10-16 19:21:46 -0300

    sram formal passing

--- formal/sram_32_sp.v
+++ formal/sram_32_sp.v
@@ -10,9 +10,9 @@ end
             mem[i] = 0;
 `endif
 
-(* anyconst *) wire [31:0] f_const_addr;
-reg [31:0] f_mem_value;
-reg [31:0] f_mem_mask;
+(* anyconst *) wire [addr_width-1:0] f_const_addr;
+reg [data_width-1:0] f_mem_value;
+reg [data_width-1:0] f_mem_mask;
 
 initial
     assume(f_mem_value == mem[f_const_addr]);
@@ -23,11 +23,11 @@ always @(posedge clock) begin
     // Handle writes
     if(en && wen && addr == f_const_addr) begin
         f_mem_mask = {{8{wmask[3]}},{8{wmask[2]}},{8{wmask[1]}},{8{wmask[0]}}};
-        f_mem_value = din & f_mem_mask;
+        f_mem_value <= (mem[f_const_addr] & ~f_mem_mask) | (din & f_mem_mask);
     end
     // Handle reads
     if(f_past_valid && $past(en) && !$past(wen) && $past(addr == f_const_addr)) begin
-        assert((dout & f_mem_mask) == f_mem_value);
+        assert(dout == f_mem_value);
     end
 end
 
---
+++ rtl/wishbone/sram_32_sp.v
@@ -0,0 +1,39 @@
+module sram_32_sp #(
+    parameter data_width = 32, // fixed
+    parameter addr_width = 8, // dynamic
+    parameter mask_width = data_width/8 // fixed
+) (
+    input                    clock,
+    input                    wen,
+    input                    en,
+    input  [mask_width-1:0]  wmask,
+    input  [addr_width-1:0]  addr,
+    input  [data_width-1:0]  din,
+    output [data_width-1:0]  dout,
+);
+    parameter length = 1 << addr_width;
+
+    `ifdef FORMAL
+        `include "formal/sram_32_sp.v"
+    `endif
+
+    reg [data_width-1:0] mem [length-1:0];
+    reg [data_width-1:0] dout;
+
+    always @(posedge clock)
+        if(en && wen) begin
+            if(wmask[0])
+                mem[addr][7:0] <= din[7:0];
+            if(wmask[1])
+                mem[addr][15:8] <= din[15:8];
+            if(wmask[2])
+                mem[addr][23:16] <= din[23:16];
+            if(wmask[3])
+                mem[addr][31:24] <= din[31:24];
+        end
+
+    always @(posedge clock)
+        if(en && !wen)
+            dout <= mem[addr];
+
+endmodule
