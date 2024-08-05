d11c2ad262512e51e70be7209fa6f6c1f7cd1552
Diego Benavente <git@eda.slmail.me>
2024-08-05 14:57:44 -0400

    Move to SV

--- rtl/cpu/copperv.sv
+++ rtl/cpu/copperv.sv
@@ -25,12 +25,12 @@ parameter imm_width         = 32;
 
 // idecoder begin
 wire [imm_width-1:0] imm;
-wire [funct_width-1:0] funct;
-wire [alu_op_width-1:0] alu_op;
+funct_e funct;
+alu_op_e alu_op;
 wire [reg_width-1:0] rd;
 wire [reg_width-1:0] rs1;
 wire [reg_width-1:0] rs2;
-wire [inst_type_width-1:0] inst_type;
+inst_type_e inst_type;
 // idecoder end
 // register_file begin
 wire rd_en;
@@ -44,7 +44,7 @@ wire [data_width-1:0] rs2_dout;
 reg [data_width-1:0] alu_din1;
 reg [data_width-1:0] alu_din2;
 wire [data_width-1:0] alu_dout;
-wire [alu_comp_width-1:0] alu_comp;
+alu_comp_e alu_comp;
 // arith_logic_unit end
 // datapath begin
 wire inst_fetch;
@@ -54,10 +54,10 @@ reg [pc_width-1:0] pc_next;
 reg [inst_width-1:0] inst;
 reg inst_valid;
 wire i_rdata_tran;
-wire [rd_din_sel_width-1:0] rd_din_sel;
-wire [pc_next_sel_width-1:0] pc_next_sel;
-wire [alu_din1_sel_width-1:0] alu_din1_sel;
-wire [alu_din2_sel_width-1:0] alu_din2_sel;
+rd_din_sel_e rd_din_sel;
+pc_next_sel_e pc_next_sel;
+alu_din1_sel_e alu_din1_sel;
+alu_din2_sel_e alu_din2_sel;
 wire store_data;
 wire load_data;
 wire [data_width-1:0] write_addr;
@@ -66,14 +66,11 @@ reg [data_width-1:0] read_data;
 reg [data_width-1:0] ext_read_data;
 reg [data_width-1:0] read_data_t;
 reg write_valid;
-wire dw_resp_tran;
-wire dw_data_addr_tran;
-wire dr_addr_tran;
+reg alu_shift_din2;
 reg read_valid;
 wire [bus_width-1:0] read_addr;
 reg [2-1:0] write_offset;
 reg [2-1:0] read_offset;
-wire dr_data_tran;
 reg [(bus_width/8)-1:0] write_strobe;
 // datapath end
 always @(posedge clk) begin
@@ -83,92 +80,68 @@ always @(posedge clk) begin
         pc <= pc_next;
     end
 end
-assign ir_addr_valid = inst_fetch;
-assign ir_addr = pc;
-assign i_rdata_tran = ir_data_valid && ir_data_ready;
+assign inst_if.stb = inst_fetch;
+assign inst_if.we = 0;
+assign inst_if.cyc = inst_fetch || inst_valid;
+assign inst_if.adr = pc;
+assign inst_if.sel = 4'b1111;
 always @(posedge clk) begin
     if(!rst) begin
         inst <= 0;
         inst_valid <= 0;
-    end else if(i_rdata_tran) begin
-        inst <= ir_data;
+    end else if(inst_if.ack) begin
+        inst <= inst_if.datrd;
         inst_valid <= 1;
     end else begin
         inst_valid <= 0;
     end
 end
-// Write response
-assign dw_resp_tran = dw_resp_valid && dw_resp_ready;
-always @(posedge clk) begin
-    if(!rst) begin
-        write_valid <= 0;
-    end else if(dw_resp_tran) begin
-        case(dw_resp)
-            data_write_resp_fail: write_valid <= 0;
-            data_write_resp_ok: write_valid <= 1;
-        endcase
-    end else
-        write_valid <= 0;
-end
-always @(posedge clk)
-    if(!rst) begin
-        ir_data_ready <= 1;
-    end
-// Write data address
-assign write_addr = alu_dout;
-assign dw_data_addr_tran = store_data && dw_data_addr_ready;
-always @(posedge clk) begin
-    if(!rst) begin
-        dw_addr <= 0;
-        dw_data <= 0;
-        dw_strobe <= 0;
-        dw_data_addr_valid <= 0;
-    end else if(dw_data_addr_tran) begin
-        dw_addr <= {write_addr[data_width-1:2],2'b0};
-        dw_data <= write_data;
-        dw_strobe <= write_strobe;
-        dw_data_addr_valid <= 1;
-    end else
-        dw_data_addr_valid <= 0;
-end
-always @(posedge clk)
-    if(!rst) begin
-        dw_resp_ready <= 1;
-    end
-// Read address
+// Data bus:
 assign read_addr = alu_dout;
-assign dr_addr_tran = load_data && dr_addr_ready;
+assign write_addr = alu_dout;
 always @(posedge clk) begin
-    if(load_data)
-        read_offset <= read_addr[1:0];
+  if(rst) begin
+    data_if.stb <= 0;
+    data_if.we <= 0;
+  end else if(store_data) begin
+    data_if.adr <= {write_addr[data_width-1:2],2'b0};
+    data_if.datwr <= write_data;
+    data_if.sel <= write_strobe;
+    data_if.stb <= 1;
+    data_if.we <= 1;
+  end else if(load_data) begin
+    data_if.adr <= {read_addr[data_width-1:2],2'b0};
+    data_if.sel <= 4'b1111;
+    data_if.stb <= 1;
+    data_if.we <= 0;
+  end else
+    data_if.stb <= 0;
+    data_if.we <= 0;
+end
+reg past_we;
+always @(posedge clk) begin
+  past_we <= data_if.we;
+  if(load_data)
+    read_offset <= read_addr[1:0];
 end
 always @(posedge clk) begin
-    if(!rst) begin
-        dr_addr <= 0;
-        dr_addr_valid <= 0;
-    end else if(dr_addr_tran) begin
-        dr_addr <= {read_addr[data_width-1:2],2'b0};
-        dr_addr_valid <= 1;
-    end else
-        dr_addr_valid <= 0;
-end
-// Read data
-assign dr_data_tran = dr_data_valid && dr_data_ready;
-always @(posedge clk) begin
-    if(!rst) begin
-        read_data <= 0;
-        read_valid <= 0;
-    end else if(dr_data_tran) begin
-        read_data <= dr_data;
-        read_valid <= 1;
-    end else begin
-        read_valid <= 0;
-    end
+  if(rst) begin
+    read_data <= 0;
+    read_valid <= 0;
+  end else if(data_if.ack && !past_we) begin
+    read_data <= data_if.datrd;
+    read_valid <= 1;
+  end else begin
+    read_valid <= 0;
+  end
 end
 always @(posedge clk) begin
-    if(!rst) begin
-        dr_data_ready <= 1;
-    end
+  if(rst) begin
+    write_valid <= 0;
+  end else if(data_if.ack && past_we) begin
+    write_valid <= 1;
+  end else
+    write_valid <= 0;
 end
 always @(*) begin
     write_offset = write_addr[1:0];
@@ -225,6 +198,8 @@ always @(*) begin
         alu_din2_sel_imm:     alu_din2 = imm;
         alu_din2_sel_const_4: alu_din2 = 4;
     endcase
+    if (alu_shift_din2)
+      alu_din2 = alu_din2[alu_shift_din2_width-1:0];
 end
 always @(*) begin
     pc_next = 0;
@@ -266,24 +241,8 @@ arith_logic_unit alu (
     .alu_comp(alu_comp)
 );
 control_unit control (
-    .clk(clk),
-    .rst(rst),
     .data_valid(write_valid || read_valid),
-    .inst_valid(inst_valid),
-    .alu_comp(alu_comp),
-    .funct(funct),
-    .inst_type(inst_type),
-    .inst_fetch(inst_fetch),
-    .rd_en(rd_en),
-    .rs1_en(rs1_en),
-    .rs2_en(rs2_en),
-    .rd_din_sel(rd_din_sel),
-    .pc_next_sel(pc_next_sel),
-    .alu_din1_sel(alu_din1_sel),
-    .alu_din2_sel(alu_din2_sel),
-    .alu_op(alu_op),
-    .store_data(store_data),
-    .load_data(load_data)
+    .*
 );
 endmodule
 
--- rtl/cpu/execution.v
+++ rtl/cpu/execution.v
@@ -2,33 +2,37 @@
 
 `define UNSIGNED(x,wlhs,high,low) {{(wlhs-(high-low+1)){1'b0}},x[high:low]}
 
-module arith_logic_unit (
+module arith_logic_unit #(
+    parameter data_width = 32,
+    parameter alu_op_width = 32,
+    parameter alu_comp_width = 32
+  ) (
     input [data_width-1:0] alu_din1,
     input [data_width-1:0] alu_din2,
     input [alu_op_width-1:0] alu_op,
     output reg [data_width-1:0] alu_dout,
     output reg [alu_comp_width-1:0] alu_comp
-);
-always @(*) begin
-    alu_dout = 0;
-    case (alu_op)
-        alu_op_nop:  alu_dout = {data_width{1'bx}};
-        alu_op_add:  alu_dout = alu_din1 + alu_din2; 
-        alu_op_sub:  alu_dout = alu_din1 - alu_din2;
-        alu_op_and:  alu_dout = alu_din1 & alu_din2;
-        alu_op_sll:  alu_dout = alu_din1 << alu_din2[alu_shift_din2_width-1:0];
-        alu_op_srl:  alu_dout = alu_din1 >> alu_din2[alu_shift_din2_width-1:0];
-        alu_op_sra:  alu_dout = $signed(alu_din1) >>> alu_din2[alu_shift_din2_width-1:0];
-        alu_op_xor:  alu_dout = alu_din1 ^ alu_din2;
-        alu_op_or:   alu_dout = alu_din1 | alu_din2;
-        alu_op_slt:  alu_dout = `UNSIGNED(alu_comp,32,alu_comp_lt,alu_comp_lt);
-        alu_op_sltu: alu_dout = `UNSIGNED(alu_comp,32,alu_comp_ltu,alu_comp_ltu);
-    endcase
-end
-always @(*) begin
-    alu_comp[alu_comp_eq]  = alu_din1 == alu_din2;
-    alu_comp[alu_comp_lt]  = $signed(alu_din1) < $signed(alu_din2);
-    alu_comp[alu_comp_ltu] = alu_din1 < alu_din2;
-end
+  );
+  always @(*) begin
+      alu_dout = 0;
+      case (alu_op)
+          alu_op_nop:  alu_dout = {data_width{1'bx}};
+          alu_op_add:  alu_dout = alu_din1 + alu_din2; 
+          alu_op_sub:  alu_dout = alu_din1 - alu_din2;
+          alu_op_and:  alu_dout = alu_din1 & alu_din2;
+          alu_op_sll:  alu_dout = alu_din1 << alu_din2;
+          alu_op_srl:  alu_dout = alu_din1 >> alu_din2;
+          alu_op_sra:  alu_dout = $signed(alu_din1) >>> alu_din2;
+          alu_op_xor:  alu_dout = alu_din1 ^ alu_din2;
+          alu_op_or:   alu_dout = alu_din1 | alu_din2;
+          alu_op_slt:  alu_dout = `UNSIGNED(alu_comp,32,alu_comp_lt,alu_comp_lt);
+          alu_op_sltu: alu_dout = `UNSIGNED(alu_comp,32,alu_comp_ltu,alu_comp_ltu);
+      endcase
+  end
+  always @(*) begin
+      alu_comp[alu_comp_eq]  = alu_din1 == alu_din2;
+      alu_comp[alu_comp_lt]  = $signed(alu_din1) < $signed(alu_din2);
+      alu_comp[alu_comp_ltu] = alu_din1 < alu_din2;
+  end
 endmodule
 
