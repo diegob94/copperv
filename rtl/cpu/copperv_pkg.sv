5578c42313a3a10a1a02338e76417a3ee3e81586
Diego Benavente <git@eda.slmail.me>
2024-08-05 13:18:04 -0400

    Create copperv pkg for enums

--- rtl/cpu/copperv_pkg.sv
+++ rtl/cpu/copperv_pkg.sv
@@ -1,9 +1,4 @@
 package copperv_pkg;
-// datapath
-parameter data_width              = 32;
-parameter pc_width                = 32;
-parameter bus_width               = 32;
-parameter bus_resp_width          = 1;
 
   typedef enum {
     imm,
@@ -34,13 +29,6 @@ parameter bus_resp_width          = 1;
     ok
   } data_write_resp_e;
 
-// regfile
-parameter reg_width         = 5;
-parameter reg_t3            = 28;
-
-// alu
-parameter alu_shift_din2_width = 5;
-
   typedef enum {
     nop,
     add,
@@ -61,10 +49,6 @@ parameter alu_shift_din2_width = 5;
     ltu
   } alu_comp_e;
 
-// idecoder
-parameter inst_width        = 32;
-parameter imm_width         = 32;
-
   typedef enum {
     imm,
     int_imm,
@@ -78,44 +62,43 @@ parameter imm_width         = 32;
     fence
   } inst_type_e;
 
-parameter funct_width       = 5;
-parameter funct_add         = 0;
-parameter funct_sub         = 1;
-parameter funct_and         = 2;
-parameter funct_eq          = 3;
-parameter funct_neq         = 4;
-parameter funct_lt          = 5;
-parameter funct_gte         = 6;
-parameter funct_ltu         = 7;
-parameter funct_gteu        = 8;
-parameter funct_mem_byte    = 9;
-parameter funct_mem_hword   = 10;
-parameter funct_mem_word    = 11;
-parameter funct_mem_byteu   = 12;
-parameter funct_mem_hwordu  = 13;
-parameter funct_jal         = 14;
-parameter funct_sll         = 15;
-parameter funct_slt         = 16;
-parameter funct_sltu        = 17;
-parameter funct_xor         = 18;
-parameter funct_srl         = 19;
-parameter funct_sra         = 20;
-parameter funct_or          = 21;
+  typedef enum {
+    add,
+    sub,
+    and,
+    eq,
+    neq,
+    lt,
+    gte,
+    ltu,
+    gteu,
+    mem_byte,
+    mem_hword,
+    mem_word,
+    mem_byteu,
+    mem_hwordu,
+    jal,
+    sll,
+    slt,
+    sltu,
+    xor,
+    srl,
+    sra,
+    or
+  } funct_e;
 
-parameter opcode_width      = 7;
-parameter opcode_load       = {5'h00, 2'b11};
-parameter opcode_fence      = {5'h03, 2'b11};
-parameter opcode_int_imm    = {5'h04, 2'b11};
-parameter opcode_auipc      = {5'h05, 2'b11};
-parameter opcode_store      = {5'h08, 2'b11};
-parameter opcode_int_reg    = {5'h0c, 2'b11};
-parameter opcode_lui        = {5'h0d, 2'b11};
-parameter opcode_branch     = {5'h18, 2'b11};
-parameter opcode_jalr       = {5'h19, 2'b11};
-parameter opcode_jal        = {5'h1b, 2'b11};
-
-parameter funct3_width      = 3;
-parameter funct7_width      = 7;
+  typedef enum {
+    load    = {5'h00, 2'b11},
+    fence   = {5'h03, 2'b11},
+    int_imm = {5'h04, 2'b11},
+    auipc   = {5'h05, 2'b11},
+    store   = {5'h08, 2'b11},
+    int_reg = {5'h0c, 2'b11},
+    lui     = {5'h0d, 2'b11},
+    branch  = {5'h18, 2'b11},
+    jalr    = {5'h19, 2'b11},
+    jal     = {5'h1b, 2'b11}
+  } opcode_e;
 
   // control_unit
   typedef enum {
@@ -126,4 +109,5 @@ parameter funct7_width      = 7;
     exec,
     mem
   } state_e;
+
 endpackage : copperv_pkg
